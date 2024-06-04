5eed70e0c91dfdbfed3432fd20f46c909080eecc
prathamm3786 <155967795+prathamm3786@users.noreply.github.com>
2024-06-04 17:56:07 +0530

    first commit

---
+++ javascript Exam/assets/js/adcart.js
@@ -0,0 +1,93 @@
+const products = [
+    {
+        id: 1,
+        name: "Funskool teddy",
+        price: "$20.00",
+        image: "assets/image/1.png",
+    },
+    {
+        id: 2,
+        name: "Baby Play Sets",
+        price: "$35.20",
+        image: "assets/image/2.png",
+    },
+    {
+        id: 3,
+        name: "Jigsaw Puzzles For Kids",
+        price: "$30",
+        image: "assets/image/3.png",
+    },
+    {
+        id: 4,
+        name: "Abstract Girl Dress",
+        price: "$25.00",
+        image: "assets/image/4.png",
+    },
+    {
+        id: 5,
+        name: "Bruder Toys Mini Ships",
+        price: "$30.00",
+        image: "assets/image/5.png",
+    },
+    {
+        id: 6,
+        name: "Abstract Boy Dress",
+        price: "$25.00",
+        image: "assets/image/6.png",
+    },
+    {
+        id: 7,
+        name: "Funskool Teddy Pink",
+        price: "$32.20",
+        image: "assets/image/7.png",
+    },
+    {
+        id: 8,
+        name: "Toys Box For Baby",
+        price: "$20.00",
+        image: "assets/image/9.png",
+    },
+    {
+        id: 9,
+        name: "Funskool Teddy Brown",
+        price: "$21.00",
+        image: "assets/image/10.png",
+    },
+    {
+        id: 10,
+        name: "Newborn Kit Set",
+        price: "$19.50",
+        image: "assets/image/11.png",
+    },
+    {
+        id: 11,
+        name: "Classic Fisher Gift",
+        price: "$50.00",
+        image: "assets/image/8.png",
+    },
+    {
+        id: 12,
+        name: "Sassy Crib and Floor Mirror",
+        price: "$25.00",
+        image: "assets/image/2 (1).png",
+    },
+    {
+        id: 13,
+        name: "Tracktor",
+        price: "$20.00",
+        image: "assets/image/2 (2).png",
+    },
+    {
+        id: 14,
+        name: "Funskool teddy",
+        price: "$19.50",
+        image: "assets/image/3 (1).png",
+    },
+    {
+        id: 15,
+        name: "Toy Ship",
+        price: "$25.00",
+        image: "assets/image/5 (1).png",
+    },
+
+];
